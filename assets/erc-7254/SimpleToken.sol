70f31aa3299cd548f29e1761e7f6c491c37264b9
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-19 17:44:44 +0000

    Add SimpleToken contract with mint and burn functionality

---
+++ assets/erc-7254/SimpleToken.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    
+    mapping(address => uint256) public balanceOf;
+    
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Mint(address indexed to, uint256 value);
+    event Burn(address indexed from, uint256 value);
+    
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _initialSupply * 10 ** uint256(_decimals);
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+    
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        require(_to != address(0), "Invalid recipient");
+        
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+    
+    function mint(address _to, uint256 _value) public {
+        require(_to != address(0), "Invalid recipient");
+        
+        uint256 adjustedValue = _value * 10 ** uint256(decimals);
+        totalSupply += adjustedValue;
+        balanceOf[_to] += adjustedValue;
+        
+        emit Mint(_to, adjustedValue);
+        emit Transfer(address(0), _to, adjustedValue);
+    }
+    
+    function burn(uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        
+        totalSupply -= _value;
+        balanceOf[msg.sender] -= _value;
+        
+        emit Burn(msg.sender, _value);
+        emit Transfer(msg.sender, address(0), _value);
+    }
+}
