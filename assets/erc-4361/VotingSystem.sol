3a7ca470a7fdefda810891d8c9a2e32b8b961fe2
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-20 23:06:14 +0000

    Implement a simple voting system with vote casting and result retrieval functions

---
+++ assets/erc-4361/VotingSystem.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.8.0;
+
+contract VotingSystem {
+    struct Proposal {
+        string name;
+        uint voteCount;
+    }
+
+    Proposal[] public proposals;
+    mapping(address => bool) public hasVoted;
+    address public owner;
+
+    constructor(string[] memory proposalNames) {
+        owner = msg.sender;
+        for (uint i = 0; i < proposalNames.length; i++) {
+            proposals.push(Proposal({
+                name: proposalNames[i],
+                voteCount: 0
+            }));
+        }
+    }
+
+    function vote(uint proposalIndex) external {
+        require(!hasVoted[msg.sender], "Already voted");
+        require(proposalIndex < proposals.length, "Invalid proposal index");
+
+        hasVoted[msg.sender] = true;
+        proposals[proposalIndex].voteCount += 1;
+    }
+
+    function winningProposal() public view returns (uint winningProposalIndex) {
+        uint winningVoteCount = 0;
+        for (uint i = 0; i < proposals.length; i++) {
+            if (proposals[i].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[i].voteCount;
+                winningProposalIndex = i;
+            }
+        }
+    }
+
+    function winnerName() external view returns (string memory winnerName_) {
+        winnerName_ = proposals[winningProposal()].name;
+    }
+
+    function getProposalCount() external view returns (uint) {
+        return proposals.length;
+    }
+}
