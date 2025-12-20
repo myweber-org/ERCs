9548a6b2236c7d010012edc4ef4139a7a11d8090
InkspillVerse <inkspill.verse@outlook.com>
2025-12-20 21:57:09 +0000

    Implement decentralized voting contract with vote delegation and result tallying

---
+++ assets/erc-6604/DecentralizedVoting.sol
@@ -0,0 +1,102 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract DecentralizedVoting {
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+    
+    struct Voter {
+        bool voted;
+        uint vote;
+        address delegate;
+        uint weight;
+    }
+    
+    address public chairperson;
+    mapping(address => Voter) public voters;
+    Proposal[] public proposals;
+    
+    event VoteCast(address indexed voter, uint proposalIndex);
+    event VoteDelegated(address indexed from, address indexed to);
+    
+    constructor(string[] memory proposalDescriptions) {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 1;
+        
+        for (uint i = 0; i < proposalDescriptions.length; i++) {
+            proposals.push(Proposal({
+                description: proposalDescriptions[i],
+                voteCount: 0
+            }));
+        }
+    }
+    
+    function giveRightToVote(address voter) external {
+        require(msg.sender == chairperson, "Only chairperson can give voting rights");
+        require(!voters[voter].voted, "The voter already voted");
+        require(voters[voter].weight == 0, "Voter already has voting rights");
+        
+        voters[voter].weight = 1;
+    }
+    
+    function delegate(address to) external {
+        Voter storage sender = voters[msg.sender];
+        require(!sender.voted, "You already voted");
+        require(to != msg.sender, "Self-delegation is disallowed");
+        
+        while (voters[to].delegate != address(0)) {
+            to = voters[to].delegate;
+            require(to != msg.sender, "Found loop in delegation");
+        }
+        
+        sender.voted = true;
+        sender.delegate = to;
+        
+        Voter storage delegate_ = voters[to];
+        if (delegate_.voted) {
+            proposals[delegate_.vote].voteCount += sender.weight;
+        } else {
+            delegate_.weight += sender.weight;
+        }
+        
+        emit VoteDelegated(msg.sender, to);
+    }
+    
+    function vote(uint proposal) external {
+        Voter storage sender = voters[msg.sender];
+        require(sender.weight != 0, "Has no right to vote");
+        require(!sender.voted, "Already voted");
+        
+        sender.voted = true;
+        sender.vote = proposal;
+        proposals[proposal].voteCount += sender.weight;
+        
+        emit VoteCast(msg.sender, proposal);
+    }
+    
+    function winningProposal() public view returns (uint winningProposal_) {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
+    
+    function winnerName() external view returns (string memory winnerName_) {
+        winnerName_ = proposals[winningProposal()].description;
+    }
+    
+    function getProposalCount() external view returns (uint) {
+        return proposals.length;
+    }
+    
+    function getProposalDetails(uint index) external view returns (string memory description, uint voteCount) {
+        require(index < proposals.length, "Invalid proposal index");
+        Proposal storage proposal = proposals[index];
+        return (proposal.description, proposal.voteCount);
+    }
+}
